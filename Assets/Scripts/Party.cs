54650b990d2750d6c5e62525d22dcb9476bd9575
Daniel Allan <talast@gmail.com>
2019-04-08 11:38:07 +1000

    Started work on a battle system, created a basic gui if I can get it working standalone and then implement it in context with the game.

---
+++ Assets/Scripts/Party.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class holds a list of monsters that are currently in the party
+// currently set a max party size but maybe this should be somewhere else    jrv
+public class Party
+{
+    public const int MAX_PARTY_MEMBERS = 4;
+    List<Monster> partyMembers;
+
+    public Party()
+    {
+        partyMembers = new List<Monster>();
+    }
+
+    public void addMember(Monster mon)
+    {
+        if(partyMembers.Count < MAX_PARTY_MEMBERS)
+            partyMembers.Add(mon);
+    }
+
+    public void removeMember(Monster mon)
+    {
+        partyMembers.Remove(mon);
+    }
+}
